3d3d03feb6d4eee0ab777f88d1dbbfd2b8c203d6
DkPanseriya <160492376+DkPanseriya@users.noreply.github.com>
2024-02-20 10:15:16 +0100

    VideoTestingScript.m

--- VideoTestingScript.m
+++ VideoTestingScript.m
@@ -47,27 +47,3 @@ end
 % Close video player
 release(depVideoPlayer);
 
-% Evaluate results
-threshold = 0.7;
-testLabels = cell(totalFrames, 2);
-testLabels(:, 1) = {'BlueCone'};
-testLabels(:, 2) = {'YellowCone'};
-
-[ap, recall, precision] = evaluateDetectionPrecision(results, testLabels, threshold);
-[am, fppi, missRate] = evaluateDetectionMissRate(results, testLabels, threshold);
-
-% Plot Precision-Recall curve
-figure;
-plot(recall{1,1}, precision{1,1}, 'r-', recall{2,1}, precision{2,1}, 'b:');
-xlabel('Recall');
-ylabel('Precision');
-legend('BlueCone', 'YellowCone');
-title('Precision-Recall Curve');
-
-% Plot Miss Rate vs False Positives per Image (FPPi) curve
-figure;
-loglog(fppi{1,1}, missRate{1,1}, 'r-', fppi{2,1}, missRate{2,1}, 'b:');
-xlabel('False Positives per Image (FPPi)');
-ylabel('Miss Rate');
-legend('BlueCone', 'YellowCone');
-title('Miss Rate vs FPPi Curve');
